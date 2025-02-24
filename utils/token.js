e43c5c9b727dd1f77fba2444229505b2636c70c3
HarshWebCraft <harshwebdev26@gmail.com>
2025-02-24 16:09:54 +0530

    initial

---
+++ utils/token.js
@@ -0,0 +1,10 @@
+const jwt = require("jsonwebtoken");
+const { ACCESS_TOKEN_SECRET } = process.env;
+
+const createAccessToken = (payload) => {
+  return jwt.sign(payload, "Rj2S?RVe9[]8-dCS6A**&b5Tsg$gwbg~Bd{*QTK");
+};
+
+module.exports = {
+  createAccessToken,
+};
